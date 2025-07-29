9630923e8fa5507cdd1f813418a538f2e819d571
Joelson87 <joelsonfelix87@gmail.com>
2025-07-29 10:46:07 -0300

    menu e main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
-
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    carregarDoArquivo();
+    int opcao;
+
+    do {
+        cout << "\n--- Menu ---\n";
+        cout << "1. Agendar Paciente\n";
+        cout << "2. Editar Paciente\n";
+        cout << "3. Excluir Paciente\n";
+        cout << "4. Listar Paciente\n";
+        cout << "5. Sair\n";
+        cout << "Escolha uma opção: ";
+        cin >> opcao;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        switch (opcao) {
+            case 1: agendarPaciente(); break;
+            case 2: editarPaciente(); break;
+            case 3: excluirPaciente(); break;
+            case 4: listarPaciente(); break;
+            case 5: cout << "Saindo...\n"; break;
+            default: cout << "Opção inválida!\n";
+        }
+    } while (opcao != 5);
+
     return 0;
 }
--- menu.cpp
+++ menu.cpp
@@ -11,8 +11,8 @@ struct Paciente {
     string procedimento;
     string idade;
     string telefone;
-    char data:
-    char horario;
+    string data;
+    string horario;
     string medico;
     string convenio;
     string particular; 
@@ -120,32 +120,4 @@ void excluirPaciente() {
     } else {
         cout << "Índice inválido.\n";
     }
-}
-
-int main() {
-    carregarDoArquivo();
-    int opcao;
-
-    do {
-        cout << "\n--- Menu ---\n";
-        cout << "1. Agendar Paciente\n";
-        cout << "2. Editar Paciente\n";
-        cout << "3. Excluir Paciente\n";
-        cout << "4. Listar Paciente\n";
-        cout << "5. Sair\n";
-        cout << "Escolha uma opção: ";
-        cin >> opcao;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-        switch (opcao) {
-            case 1: agendarPaciente(); break;
-            case 2: editarPaciente(); break;
-            case 3: excluirPaciente(); break;
-            case 4: listarPaciente(); break;
-            case 5: cout << "Saindo...\n"; break;
-            default: cout << "Opção inválida!\n";
-        }
-    } while (opcao != 5);
-
-    return 0;
 }
