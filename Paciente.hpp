19c794d3030a66d093b0b9a3a630b784ee10752d
MARIA AMANDA <massilvaa21@gmail.com>
2025-07-30 09:56:32 -0300

    class

--- Paciente.hpp
+++ Paciente.hpp
@@ -3,39 +3,58 @@
 #include "menu.cpp"
 #pragma once
 
-class Medico:Paciente{
-    private:
-    string especializacao;
-    void setespecializacao();
-    string getespecializacao();
+using namespace std;
 
-};
-class Paciente {
-    private: 
+// Classe Pessoa
+class Pessoa {
+
+private:
     string nome;
     int idade;
+
+public:
+    Pessoa(string n = "", int i = 0) : nome(n), idade(i) {}
+
+    void setNome(string n) { nome = n; }
+    void setIdade(int i) { idade = i; }
+
+    string getNome() const { return nome; }
+    int getIdade() const { return idade; }
+};
+
+// Classe Paciente 
+class Paciente : public Pessoa {
+private:
     string telefone;
     bool convenio;
-    bool particular; 
-    Paciente();
+    bool particular;
 
-    void setnome(string a);
-    void setidade(int a);
-    void settelefone(string a);
-    void setparticular(string a);
-    string getnome();
-    int getidade();
-    string gettelefone();
+public:
+    Paciente(string n = "", int i = 0, string t = "", bool c = false, bool p = false)
+        : Pessoa(n, i), telefone(t), convenio(c), particular(p) {}
+        
+    void setTelefone(string t) { telefone = t; }
+    void setConvenio(bool c) { convenio = c; }
+    void setParticular(bool p) { particular = p; }
+
+    string getTelefone() const { return telefone; }
+    bool getConvenio() const { return convenio; }
+    bool getParticular() const { return particular; }
 };
-int Paciente::getidade(){
-    return idade;
-}
-void Paciente::setidade(int a){
-    idade = a;
-}
-void Paciente::settelefone(string a){
-    telefone = a;
-}
-void Paciente::setnome(string a){
-    nome = a;
-}
+
+class Medico: public Pessoa{
+    private:
+    string especializacao;
+    string CRM;
+
+public:
+    Medico(string n = "", int i = 0, string e = "", string c = "")
+        : Pessoa(n, i), especializacao(e), CRM(c) {}
+
+    void setEspecializacao(string e) { especializacao = e; }
+    void setCRM(string c) { CRM = c; }
+
+    string getEspecializacao() const { return especializacao; }
+    string getCRM() const { return CRM; }
+};       
+
